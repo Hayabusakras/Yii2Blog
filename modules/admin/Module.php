3485f5684bcccb4ea42e3bee303ee2929391d10d
Hayabusakras <Hayabusakras@gmail.com>
2017-11-03 03:07:42 +0200

    admin view modified

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -10,7 +10,7 @@ class Module extends \yii\base\Module
     /**
      * @inheritdoc
      */
-    public $layouts = '/admin';
+    public $layout = '/admin';
 
     public $controllerNamespace = 'app\modules\admin\controllers';
 
